c01e4f654c73c36a956e80f8088ca55c241a1ba6
Raisadz <rdzhamtyrova@protonmail.com>
2022-11-24 18:41:06 +0000

    add fastapi tests

---
+++ starter/test_main_app.py
@@ -0,0 +1,58 @@
+from fastapi.testclient import TestClient
+from starter.main import app
+import pytest
+import json
+import requests
+
+client = TestClient(app)
+
+def test_get():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"greeting":"Welcome!"}
+
+@pytest.fixture
+def salary_low():
+    return {
+    "age": 30,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "Black",
+    "sex": "Female",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "Cuba",
+    "salary": "<=50K"
+    }
+    
+@pytest.fixture
+def salary_high():
+    return {'age': 30,
+    'workclass': 'Private',
+    'fnlgt': 167309,
+    'education': 'Bachelors',
+    'education-num': 13,
+    'marital-status': 'Married-civ-spouse',
+    'occupation': 'Adm-clerical',
+    'relationship': 'Husband',
+    'race': 'White',
+    'sex': 'Male',
+    'capital-gain': 0,
+    'capital-loss': 1902,
+    'hours-per-week': 40,
+    'native-country': 'United-States',
+    'salary': '>50K'}
+
+def test_post(salary_low, salary_high):
+    r_low = requests.post('http://0.0.0.0:5000/inference/',  data=json.dumps(salary_low))
+    r_high = requests.post('http://0.0.0.0:5000/inference/',  data=json.dumps(salary_high))
+    assert r_low.status_code == 200
+    assert r_high.status_code == 200
+    assert r_low.json() == ["<=50K"]    
+    assert r_high.json() == [">50K"]
